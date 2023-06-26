3e4936143092227bc2020ec13ad209b23290dfa3
jeffBoom <jeff@boomreturns.com>
2023-06-26 17:15:05 -0400

    nav

--- src/components/jdNavItem/jdNavItem.tsx
+++ src/components/jdNavItem/jdNavItem.tsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
-import { IJdRouteObj } from "../../utils/jdRoutes";
+import { Link, useLocation, Location } from "react-router-dom";
+import jdRoutes, { IJdRouteObj } from "../../utils/jdRoutes";
 import styles from './navItemStyles.module.scss';
+import { useEffect, useState } from "react";
 
-export function JdNavItem(props: IJdRouteObj & { isActive: boolean }): React.JSX.Element {
+export function JdNavItem(props: IJdRouteObj): React.JSX.Element {
+
+    // Location changes
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const location: Location = useLocation();
+    useEffect(() => {
+        setIsActive(props.path === location.pathname);
+    }, [location]);
+
     return (
-        <Link to={props.path!} className={`${styles.navItemContainer} ${props.isActive ? styles.navItemActive : ''}`}>
+        <Link to={props.path!} className={`${styles.navItemContainer} ${isActive ? styles.navItemActive : ''}`}>
 
             {/* ICON */}
             <div className={styles.navItemIcon}>
