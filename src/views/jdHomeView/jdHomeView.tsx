975cfdf921676520ca3897aa120e1cebb4eff0e6
jeffBoom <jeff@boomreturns.com>
2023-06-26 14:18:38 -0400

    home view

--- src/views/jdHomeView/jdHomeView.tsx
+++ src/views/jdHomeView/jdHomeView.tsx
@@ -5,14 +5,12 @@ import { ReactComponent as HomeBlob } from '../../assets/svg/blobs/homeBlob.svg'
 export function JdHomeView(): React.JSX.Element {
     return (
         <JdViewWrapper>
-            <HomeBlob className={styles.homeBlob} />
 
             {/* H1 */}
             <div className={styles.homeContainer}>
                 <h1>Jeff Dayton</h1>
                 <p>👋 Good thing I don't write taglines for a living, checkout my portfolio instead.</p>
             </div>
-
 
         </JdViewWrapper>
     )
