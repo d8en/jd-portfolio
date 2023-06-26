f9128dd38bf181171572a4c2578a0934b533cbcd
jeffBoom <jeff@boomreturns.com>
2023-06-26 16:01:52 -0400

    skills

--- src/components/jdXpItem/jdXpItem.tsx
+++ src/components/jdXpItem/jdXpItem.tsx
@@ -1,8 +1,32 @@
 import styles from './xpItemStyles.module.scss';
 
-export function JdXpItem(): React.JSX.Element {
+export interface IJdXpItemProps {
+    title: string;
+    icon: React.JSX.Element;
+    xp: string;
+    fillAmt: number;
+}
+
+export function JdXpItem(props: IJdXpItemProps): React.JSX.Element {
     return (
         <div className={styles.xpItemContainer}>
+
+            {/* FIRST COLUMN  */}
+            <div className={styles.xpColumn}>
+
+                {/* ICON */}
+                <div className={styles.xpIcon}>
+                    {props.icon}
+                </div>
+
+                {/* CONNECTOR */}
+                <div className={styles.xpConnector} />
+            </div>
+
+            {/* SECOND COLUMN */}
+            <div className={styles.xpColumn}>
+                <h2>{props.title}</h2>
+            </div>
 
         </div>
     )
--- src/views/jdSkillsView/jdSkillsView.tsx
+++ src/views/jdSkillsView/jdSkillsView.tsx
@@ -1,9 +1,16 @@
 import { JdViewWrapper } from "../../components/jdViewWrapper/jdViewWrapper";
+import { JdXpItem } from "../../components/jdXpItem/jdXpItem";
+import { ReactComponent as ReactIcon } from '../../assets/svg/brands/react.svg';
 
 export function JdSkillsView(): React.JSX.Element {
     return (
         <JdViewWrapper>
-
+            <JdXpItem
+                title="React / React Native"
+                icon={<ReactIcon />}
+                xp="4 Years"
+                fillAmt={1}
+            />
         </JdViewWrapper>
     )
 }
