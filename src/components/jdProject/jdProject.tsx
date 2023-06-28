963738c72e750812c085373f985828211f3fb85e
jeffBoom <jeff@boomreturns.com>
2023-06-28 15:26:16 -0400

    projects

---
+++ src/components/jdProject/jdProject.tsx
@@ -0,0 +1,9 @@
+import styles from './projectStyles.module.scss';
+
+export function JdProject(): React.JSX.Element {
+    return (
+        <div className={styles.projectsContainer}>
+
+        </div>
+    )
+}
