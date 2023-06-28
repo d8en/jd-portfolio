c30106a8251d7db25dfe1fee66052ba60f4237a0
jeffBoom <jeff@boomreturns.com>
2023-06-28 16:29:49 -0400

    projects

--- src/stores/jdProjectStore.tsx
+++ src/stores/jdProjectStore.tsx
@@ -11,14 +11,24 @@ export class JdProjectStore {
         makeAutoObservable(this);
     }
 
-    public projects: IJdProject[] = [{
-        company: jdCompaniesStore.boomerang,
-        skills: [jdSkillsStore.typescript, jdSkillsStore.react, jdSkillsStore.css, jdSkillsStore.productDesign],
-        image: 'https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/airshopPreview.webp?alt=media',
-        component: <JdAirshopApp />,
-        title: 'Operation “Build a returns app with home pickup in one month”',
-        desc: 'Our first prospect signed with us a month away from their new store launch in Chicago. They needed a return solution that was customized and offered a new cutting edge feature - home pickup. We were still in the early stages.',
-    }];
+    public projects: IJdProject[] = [
+        {
+            company: jdCompaniesStore.boomerang,
+            skills: [jdSkillsStore.typescript, jdSkillsStore.react, jdSkillsStore.css, jdSkillsStore.sass, jdSkillsStore.productDesign],
+            image: 'https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/airshopPreview.webp?alt=media',
+            component: <JdAirshopApp />,
+            title: 'Operation “Build a returns app with home pickup in one month”',
+            desc: 'Our first prospect signed with us a month away from their new store launch in Chicago. They needed a return solution that was customized and offered a new cutting edge feature - home pickup. We were still in the early stages.',
+        },
+        {
+            company: jdCompaniesStore.boomerang,
+            skills: [jdSkillsStore.typescript, jdSkillsStore.react, jdSkillsStore.css, jdSkillsStore.mobX, jdSkillsStore.sass, jdSkillsStore.productDesign],
+            image: 'https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/shopifyApp.webp?alt=media',
+            component: <JdAirshopApp />,
+            title: 'Shopify App - Home pickup for any Shopify store owner',
+            desc: 'After we launched our Chicago based pickup solution, it was time to scale up. The Shopify ecosystem seemed like a great place to experiment with offering home pickup to a large set of reatilers.',
+        }
+    ];
 
     public get filter(): IJdSkillCo[] {
         return [];
--- src/views/jdProjectsView/jdProjectsView.tsx
+++ src/views/jdProjectsView/jdProjectsView.tsx
@@ -2,15 +2,18 @@ import { JdProject } from "../../components/jdProject/jdProject";
 import { JdViewWrapper } from "../../components/jdViewWrapper/jdViewWrapper";
 import { IJdProject } from "../../models/iJdProject";
 import jdProjectStore from "../../stores/jdProjectStore";
+import styles from './projectViewStyles.module.scss';
 
 export function JdProjectsView(): React.JSX.Element {
     return (
         <JdViewWrapper>
-            {jdProjectStore.projects.map((project: IJdProject) => {
-                return (
-                    <JdProject key={project.title} {...project} />
-                )
-            })}
+            <div className={styles.projectViewContainer}>
+                {jdProjectStore.projects.map((project: IJdProject) => {
+                    return (
+                        <JdProject key={project.title} {...project} />
+                    )
+                })}
+            </div>
         </JdViewWrapper>
     )
 }
