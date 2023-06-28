bd444d1288f438cb897a465da86c548c09858f83
jeffBoom <jeff@boomreturns.com>
2023-06-28 12:47:21 -0400

    skills class

--- src/data/jdSkills.tsx
+++ src/data/jdSkills.tsx
@@ -2,6 +2,8 @@ import { JdSkillType } from "../models/enums/jdSkillType";
 import { IJdSkill } from "../models/iJdSkill";
 import { ReactComponent as TsIcon } from '../assets/svg/brands/typescript.svg';
 import { ReactComponent as ReactIcon } from '../assets/svg/brands/react.svg';
+import { ReactComponent as SassIcon } from '../assets/svg/brands/sass.svg';
+import { ReactComponent as CssIcon } from '../assets/svg/brands/css.svg';
 
 class JdSkills {
 
@@ -24,6 +26,20 @@ class JdSkills {
         tag: 'reactnative',
         icon: <ReactIcon />,
         type: JdSkillType.ReactNative,
+    }
+
+    public mobX: IJdSkill = {
+        name: 'MobX',
+        tag: 'mobx',
+        type: JdSkillType.MobX,
+        imgSrc: 'https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/mobx.webp?alt=media',
+    }
+
+    public css: IJdSkill = {
+        name: 'CSS',
+        tag: 'css',
+        type: JdSkillType.Css,
+        icon: <CssIcon />,
     }
 }
 
