34720484f6fb5567fa572b1153571096d0ceef6a
jeffBoom <jeff@boomreturns.com>
2023-06-29 14:14:08 -0400

    project opppen

--- src/components/jdProjects/jdAirshopApp.tsx
+++ src/components/jdProjects/jdAirshopApp.tsx
@@ -1,8 +1,17 @@
+import jdStringUtils from "../../utils/jdStringUtils";
+
 export function JdAirshopApp(): React.JSX.Element {
     return (
         <>
-            <h2>Goal</h2>
+
+            {/* GOAL */}
+            <h2>{jdStringUtils.emojiGoal}Goal</h2>
             <p>Develop an application that allows end users to schedule at-home return pickups. The app should ideally provide same-day pickup with specified time windows such as morning, noon, or evening. Additionally, the app should maintain clear communication with the end user throughout the logistics process to ensure a smooth experience.</p>
+
+            {/* PROCESS */}
+            <h2>{jdStringUtils.emojiFunFeatures}Fun Features</h2>
+            <p>Develop an application that allows end users to schedule at-home return pickups. The app should ideally provide same-day pickup with specified time windows such as morning, noon, or evening. Additionally, the app should maintain clear communication with the end user throughout the logistics process to ensure a smooth experience.</p>
+
         </>
     )
 }
---
+++ src/utils/jdStringUtils.tsx
@@ -0,0 +1,11 @@
+class JdStringUtils {
+    public emojiFunFeatures: string = '🥳  ';
+    public emojiIdeation: string = '🧠  ';
+    public emojiDesign: string = '🎨  ';
+    public emojiBuild: string = '🚧  ';
+    public emojiResults: string = '📊  ';
+    public emojiGoal: string = '🎯  ';
+}
+
+const jdStringUtils = new JdStringUtils();
+export default jdStringUtils;
