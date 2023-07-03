d7991caedadb4fc18cf70bcca1eb14664bf88ba8
jeffBoom <jeff@boomreturns.com>
2023-07-03 15:33:59 -0400

    img open

--- src/components/jdImg/jdImg.tsx
+++ src/components/jdImg/jdImg.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+import styles from './imgStyles.module.scss';
+
 export function JdImg(props: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>): React.JSX.Element {
+
+    const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
+
+    const onClick = (): void => {
+        console.log('fullscreen clicked');
+        setIsFullscreen(!isFullscreen);
+    }
+
     return (
-        <img {...props} />
+        <>
+            <img onClick={onClick} {...props} />
+
+            {/* OPENED */}
+            {isFullscreen &&
+                <div
+                    className={styles.imageFullScreen}
+                    onClick={onClick}
+                >
+                    <img {...props} />
+                </div>
+            }
+        </>
     )
 }
