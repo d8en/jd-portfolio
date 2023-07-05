02a45b7ad5b22135d7737bc44c4ba9f5b81e1221
jeffBoom <jeff@boomreturns.com>
2023-07-05 11:46:15 -0400

    smart routing

--- src/components/jdProjects/jdSmartRouteApp.tsx
+++ src/components/jdProjects/jdSmartRouteApp.tsx
@@ -1,6 +1,7 @@
 import jdStringUtils from '../../utils/jdStringUtils';
+import { JdImg } from '../jdImg/jdImg';
 import { JdProjectSubHeader } from '../jdProjectSubHeader/jdProjectSubHeader';
-// import styles from './proejctsStyles.module.scss';
+import styles from './proejctsStyles.module.scss';
 
 export function JdSmartRouteApp(): React.JSX.Element {
     return (
@@ -16,8 +17,28 @@ export function JdSmartRouteApp(): React.JSX.Element {
                 <li>Browser drag and drop APIs</li>
                 <li>Typeahead drop down input (single select)</li>
                 <li>Google maps API</li>
-                <li>Latest React router hooks</li>
+                <li>React router hooks</li>
+                <li>Framer motion (lifecycle animations)</li>
             </ul>
+
+            {/* IDEATION */}
+            <JdProjectSubHeader {...jdStringUtils.ideation} />
+            <p>How could we build an app that any national retailer could use to optimize return logistics? What cusotmizations would they need? How could we optimized the return path down to the cent?</p>
+
+            <div className={styles.multiMedia}>
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/smartRouteNotes1.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/smartRouteNotes2.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/smartRouteNotes3.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+            </div>
 
         </>
     )
