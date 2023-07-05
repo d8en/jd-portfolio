0e982c59fb38ed870b1c72ea976c5ae02aa9b21c
jeffBoom <jeff@boomreturns.com>
2023-07-05 13:00:12 -0400

    smart route web

--- src/components/jdProjects/jdSmartRouteWeb.tsx
+++ src/components/jdProjects/jdSmartRouteWeb.tsx
@@ -1,5 +1,7 @@
 import jdStringUtils from "../../utils/jdStringUtils";
+import { JdImg } from "../jdImg/jdImg";
 import { JdProjectSubHeader } from "../jdProjectSubHeader/jdProjectSubHeader";
+import styles from './proejctsStyles.module.scss';
 
 export function JdSmartRouteWeb(): React.JSX.Element {
     return (
@@ -22,6 +24,40 @@ export function JdSmartRouteWeb(): React.JSX.Element {
             {/* IDEATION */}
             <JdProjectSubHeader {...jdStringUtils.ideation} />
             <p>Most of my projects are apps so it was refreshing working on a website.</p>
+
+            <p>I met with our head of marketing to brainstorm, and quickly started to put together a few concepts</p>
+
+            <div className={styles.multiMedia}>
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/smartRouteWebNotes1.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/smartRouteWebNotes2.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+            </div>
+
+            {/* CAPTION */}
+            <figcaption className={styles.projectImageCaption}>Screenshots of our whiteboarding discussions.</figcaption>
+
+            <p>I came up with a traditonal concept, inspired by many Saas products out there.</p>
+
+            <div className={styles.multiMedia}>
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/desktopProto.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+                <JdImg
+                    srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/mobileProto.webp?alt=media'
+                    className={styles.projectsImage}
+                />
+            </div>
+
+            {/* CAPTION */}
+            <figcaption className={styles.projectImageCaption}>Prototypes of the first "traditional Saas" layout.</figcaption>
+
+            <p>Our CMO had an excellent idea: lets take the user on a "route" through the old ways of doing things, then the new. I loved this idea so I created a new concept and we moved forward with it.</p>
         </>
     )
 }
