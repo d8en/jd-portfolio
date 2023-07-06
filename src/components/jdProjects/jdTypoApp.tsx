923c172ecbd8f877c7a92272da6976c75d11609d
jeffBoom <jeff@boomreturns.com>
2023-07-06 10:17:44 -0400

    typo

--- src/components/jdProjects/jdTypoApp.tsx
+++ src/components/jdProjects/jdTypoApp.tsx
@@ -1,7 +1,23 @@
+import jdStringUtils from "../../utils/jdStringUtils";
+import { JdProjectSubHeader } from "../jdProjectSubHeader/jdProjectSubHeader";
 
 
 export function JdTypoApp(): React.JSX.Element {
     return (
-        <></>
+        <>
+
+            {/* GOAL */}
+            <JdProjectSubHeader {...jdStringUtils.goal} />
+            <p>Create a text-based social media app that shows the reader exactly how the writer crafted the post.</p>
+
+            {/* FUN FEATURES */}
+            <JdProjectSubHeader {...jdStringUtils.favoriteStuff} />
+            <ul>
+                <li>Animated donut timer</li>
+                <li>Record and playback of keystrokes and selections</li>
+                <li>Minimal text input</li>
+            </ul>
+
+        </>
     )
 }
