98a8d13eccd7ce47f7d0bab52a1cad2d4bd91f38
jeffBoom <jeff@boomreturns.com>
2023-07-06 10:28:43 -0400

    typo

--- src/components/jdProjects/jdTypoApp.tsx
+++ src/components/jdProjects/jdTypoApp.tsx
@@ -1,5 +1,7 @@
 import jdStringUtils from "../../utils/jdStringUtils";
+import { JdImg } from "../jdImg/jdImg";
 import { JdProjectSubHeader } from "../jdProjectSubHeader/jdProjectSubHeader";
+import styles from './proejctsStyles.module.scss';
 
 
 export function JdTypoApp(): React.JSX.Element {
@@ -8,7 +10,7 @@ export function JdTypoApp(): React.JSX.Element {
 
             {/* GOAL */}
             <JdProjectSubHeader {...jdStringUtils.goal} />
-            <p>Create a text-based social media app that shows the reader exactly how the writer crafted the post.</p>
+            <p>Create a text-based social media app that shows the reader exactly how the post was written.</p>
 
             {/* FUN FEATURES */}
             <JdProjectSubHeader {...jdStringUtils.favoriteStuff} />
@@ -17,6 +19,18 @@ export function JdTypoApp(): React.JSX.Element {
                 <li>Record and playback of keystrokes and selections</li>
                 <li>Minimal text input</li>
             </ul>
+
+            {/* IDEATION */}
+            <JdProjectSubHeader {...jdStringUtils.ideation} />
+            <p>The only major challenge was getting familiar with how I could record text selections.</p>
+
+            <JdImg
+                srcSet='https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/typoSs1.webp?alt=media'
+                className={styles.projectsImage}
+                style={{ borderRadius: 24 }}
+            />
+
+            <p>Aside from that, I also wanted a feature that allowed the reader to adjust the playback speed of the post (ex: 2x, 4x, 8x).</p>
 
         </>
     )
