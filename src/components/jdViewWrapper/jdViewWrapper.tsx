d46a448376fa2d404c709505d4a815c164363b19
jeffBoom <jeff@boomreturns.com>
2023-07-07 16:58:01 -0400

    project nav

--- src/components/jdViewWrapper/jdViewWrapper.tsx
+++ src/components/jdViewWrapper/jdViewWrapper.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import styles from './viewWrapperStyles.module.scss';
 import jdRoutes from '../../utils/jdRoutes';
 import jdStringUtils from '../../utils/jdStringUtils';
@@ -9,12 +9,18 @@ export interface IJdViewWrapperProps {
 
 export function JdViewWrapper(props: React.PropsWithChildren<IJdViewWrapperProps>): JSX.Element {
 
+    // Scroll view ref
+    const divRef = useRef<HTMLDivElement | null>(null);
+
+    // Mount
     useEffect(() => {
         document.title = `${jdStringUtils.pageTitlePrefix}${jdRoutes.activeRoute.name}`;
+        if (divRef.current) divRef.current.scrollTop = 0;
     }, []);
 
     return (
         <div
+            ref={divRef}
             {...props.divProps}
             className={`${styles.viewContent} ${props.divProps?.className ? props.divProps.className : ''}`}
         >
