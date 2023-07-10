872382c76f1d2bac35faf859a5194fa004b5b4e0
jeffBoom <jeff@boomreturns.com>
2023-07-10 11:27:32 -0400

    landing

--- src/components/JdNav/jdNav.tsx
+++ src/components/JdNav/jdNav.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import jdRoutes, { IJdRouteObj } from '../../utils/jdRoutes';
 import { JdNavItem } from '../jdNavItem/jdNavItem';
 import styles from './navStyles.module.scss';
+import { motion } from 'framer-motion';
+import jdAniUtils from '../../utils/jdAniUtils';
 
 export function JdNav(): React.JSX.Element {
     return (
-        <div className={styles.navContainer}>
+        <motion.div
+            initial={{
+                bottom: -100,
+            }}
+            animate={{
+                bottom: 0,
+            }}
+            transition={jdAniUtils.springTransition}
+            className={styles.navContainer}
+        >
 
             {/* ROUTES */}
             <div className={styles.navRoutes}>
@@ -13,6 +24,6 @@ export function JdNav(): React.JSX.Element {
                     <JdNavItem {...route} key={route.path} />
                 )}
             </div>
-        </div>
+        </motion.div>
     );
 }
