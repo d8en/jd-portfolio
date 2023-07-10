45eebdae047cc46bb2b3bcaecc4b59d265a8da6d
jeffBoom <jeff@boomreturns.com>
2023-07-10 13:26:19 -0400

    aither

--- src/components/jdProjects/jdAether.tsx
+++ src/components/jdProjects/jdAether.tsx
@@ -1,5 +1,8 @@
 import jdStringUtils from "../../utils/jdStringUtils";
+import { JdATag } from "../jdATag/jdATag";
+import { JdImg } from "../jdImg/jdImg";
 import { JdProjectSubHeader } from "../jdProjectSubHeader/jdProjectSubHeader";
+import styles from './proejctsStyles.module.scss';
 
 export function JdAether(): React.JSX.Element {
     return (
@@ -8,6 +11,72 @@ export function JdAether(): React.JSX.Element {
             {/* GOAL */}
             <JdProjectSubHeader {...jdStringUtils.goal} />
             <p>Design an insurance brand that feels transparent and refreshing.</p>
+
+            {/* FUN FEATURES */}
+            <JdProjectSubHeader {...jdStringUtils.favoriteStuff} />
+            <ul>
+                <li>Custom typeface</li>
+                <li>Wordmark</li>
+                <li>Tagline</li>
+                <li>Style guide</li>
+                <li>Deck template</li>
+            </ul>
+
+            {/* IDEATION */}
+            <JdProjectSubHeader {...jdStringUtils.ideation} />
+            <p>Like most founders I've worked with, the <JdATag href="https://www.aitherhealth.com/">aither health</JdATag> folks had a clear vision - cut through the noise and build trust with clients.</p>
+
+            <p>We met to review their vision, expected deliverables, timelines, and compensation.</p>
+
+            <JdImg
+                className={styles.projectsImage}
+                srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/aitherNotes.webp?alt=media"
+            />
+
+            {/* CAPTION */}
+            <figcaption className={styles.projectImageCaption}>Whiteboarding notes</figcaption>
+
+            {/* DESIGN */}
+            <JdProjectSubHeader {...jdStringUtils.design} />
+            <p>I've always loved typeface design so I created their wordmark from scratch.</p>
+
+            <div className={styles.multiMedia}>
+                <JdImg
+                    className={styles.projectsImage}
+                    srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/aitherSs1.webp?alt=media"
+                />
+                <JdImg
+                    className={styles.projectsImage}
+                    srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/aitherSs3.webp?alt=media"
+                />
+            </div>
+
+            {/* CAPTION */}
+            <figcaption className={styles.projectImageCaption}>Finished brand mockups</figcaption>
+
+            <p>This project was super fast with no revisions needed so all that was left was to prepare the final assets (style guide, deck template, and logo exports).</p>
+
+            <div className={styles.multiMedia}>
+                <JdImg
+                    className={styles.projectsImage}
+                    srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/styleGuide4.webp?alt=media"
+                />
+                <JdImg
+                    className={styles.projectsImage}
+                    srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/styleGuide2.webp?alt=media"
+                />
+            </div>
+
+            <div className={styles.multiMedia}>
+                <JdImg
+                    className={styles.projectsImage}
+                    srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/styleGuide3.webp?alt=media"
+                />
+                <JdImg
+                    className={styles.projectsImage}
+                    srcSet="https://firebasestorage.googleapis.com/v0/b/jd-portfolio-334c7.appspot.com/o/styleGuide1.webp?alt=media"
+                />
+            </div>
 
         </>
     )
