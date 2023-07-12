cbb13efb85fc5e26cb66eff8ed247fef468e8eab
jeffBoom <jeff@boomreturns.com>
2023-07-12 13:27:36 -0400

    HOME

--- src/views/jdHomeView/jdHomeView.tsx
+++ src/views/jdHomeView/jdHomeView.tsx
@@ -1,5 +1,6 @@
 import { JdViewWrapper } from '../../components/jdViewWrapper/jdViewWrapper';
 import styles from './homeStyles.module.scss';
+import { ReactComponent as JdLogo } from '../../assets/svg/brands/jd.svg';
 
 export function JdHomeView(): React.JSX.Element {
     return (
@@ -7,8 +8,23 @@ export function JdHomeView(): React.JSX.Element {
 
             {/* TITLE */}
             <div className={styles.homeHeader}>
-                <h1>Jeff Dayton</h1>
+
+                {/* TITLE / LOGO */}
+                <div className={styles.homeTitle}>
+
+                    {/* LOGO */}
+                    <div className={styles.homeLogo} >
+                        <JdLogo />
+                    </div>
+
+                    {/* TITLE */}
+                    <h1>Jeff Dayton</h1>
+                </div>
+
+                {/* HEADLINE */}
                 <h2 className={styles.homeSubHeader}>👋 Hey! I'm a <strong>software engineer</strong> and <strong>product designer</strong> with 8 years of experience designing and building products.</h2>
+
+                {/* SUB HEADER */}
                 <p>Checkout my projects below!</p>
             </div>
 
