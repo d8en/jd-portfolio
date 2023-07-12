0d19b93b636c0983e21473320776719ab4903096
jeffBoom <jeff@boomreturns.com>
2023-07-12 12:49:13 -0400

    skelly

---
+++ src/components/jdSkelly/jdSkelly.tsx
@@ -0,0 +1,5 @@
+import styles from './skellyStyles.module.scss';
+
+export function JdSkelly(props: React.HTMLAttributes<HTMLDivElement>): React.JSX.Element {
+    return <div {...props} className={`${styles.skellyContainer} ${props.className}`} />
+}
