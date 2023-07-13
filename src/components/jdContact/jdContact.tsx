421027952356fef7398880928c0b3cb5a34b6fb1
jeffBoom <jeff@boomreturns.com>
2023-07-13 12:42:40 -0400

    contact

--- src/components/jdContact/jdContact.tsx
+++ src/components/jdContact/jdContact.tsx
@@ -1,10 +1,22 @@
 import styles from './contactStyles.module.scss';
 import { ReactComponent as ContactIcon } from '../../assets/svg/icons/mailOpen.svg';
+import { motion } from 'framer-motion';
+import jdAniUtils from '../../utils/jdAniUtils';
 
 export function JdContact(): React.JSX.Element {
     return (
-        <div className={styles.contactContainer}>
+        <motion.div
+            initial={{ bottom: -40, opacity: 0 }}
+            animate={{ bottom: 'calc(100% + 24px)', opacity: 1 }}
+            transition={{ ...jdAniUtils.baseEase, delay: 3 }}
+            className={styles.contactContainer}
+        >
+
+            {/* ICON */}
             <ContactIcon style={{ marginBottom: 3 }} />
-        </div>
+
+            {/* TEXT */}
+            <span>contact</span>
+        </motion.div>
     )
 }
