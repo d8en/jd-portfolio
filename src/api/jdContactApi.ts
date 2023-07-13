c2231c3237fb6a5896f5d7e3a403a0e01ea79824
jeffBoom <jeff@boomreturns.com>
2023-07-13 15:31:20 -0400

    contact

--- src/api/jdContactApi.ts
+++ src/api/jdContactApi.ts
@@ -1,4 +1,24 @@
+import jdContactManager from "../managers/jdContactManager";
+
 class JdContactApi {
+
+    private url: string = 'http://localhost:3000/api/v1/contact';
+
+    public async sendForm(): Promise<boolean> {
+        try {
+            await fetch(this.url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(jdContactManager.store.contactInfo),
+            });
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
 
 }
 const jdContactApi: JdContactApi = new JdContactApi();
