ed75ae7df170fd224e3373b9ea4a96799c4b1267
jeffBoom <jeff@boomreturns.com>
2023-07-13 13:31:57 -0400

    contact

--- src/components/jdContact/jdContact.tsx
+++ src/components/jdContact/jdContact.tsx
@@ -1,24 +1,30 @@
 import styles from './contactStyles.module.scss';
 import { ReactComponent as ContactIcon } from '../../assets/svg/icons/mailOpen.svg';
+import { ReactComponent as CloseIcon } from '../../assets/svg/icons/close.svg';
 import { motion } from 'framer-motion';
 import jdAniUtils from '../../utils/jdAniUtils';
 import jdContactManager from '../../managers/jdContactManager';
+import { observer } from 'mobx-react-lite';
 
-export function JdContact(): React.JSX.Element {
+export const JdContact = observer(() => {
     return (
         <motion.div
             initial={{ bottom: -40, opacity: 0 }}
             animate={{ bottom: 95, opacity: 1 }}
             transition={{ ...jdAniUtils.baseEase, delay: 2.2 }}
             className={styles.contactContainer}
-            onClick={() => jdContactManager.setStateAsync({ isOpen: true })}
+            onClick={() => jdContactManager.setStateAsync({ isOpen: !jdContactManager.store.isOpen })}
         >
 
             {/* ICON */}
-            <ContactIcon style={{ marginBottom: 3 }} />
+            {jdContactManager.store.isOpen ?
+                <CloseIcon />
+                :
+                <ContactIcon style={{ marginBottom: 3 }} />
+            }
 
             {/* TEXT */}
             <span>contact</span>
         </motion.div>
     )
-}
+});
