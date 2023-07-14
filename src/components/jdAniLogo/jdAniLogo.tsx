6c62a4b5e6b48d4395228a793e3f3fa43d1c5d80
jeffBoom <jeff@boomreturns.com>
2023-07-13 22:48:25 -0400

    animation fixes

--- src/components/jdAniLogo/jdAniLogo.tsx
+++ src/components/jdAniLogo/jdAniLogo.tsx
@@ -5,12 +5,12 @@ import { ReactComponent as JdLogo } from '../../assets/svg/brands/jd.svg';
 export function JdAniLogo(props: MotionProps): React.JSX.Element {
     return (
         <motion.div
-            initial={{ bottom: '-100vh', opacity: 0, scale: 1.2 }}
+            initial={{ bottom: '-100vh', opacity: 0 }}
             transition={{
                 ease: cubicBezier(0, 1, 0, 1),
                 duration: 1,
             }}
-            animate={{ position: 'relative', bottom: 0, opacity: 1, scale: 1 }}
+            animate={{ position: 'relative', bottom: 0, opacity: 1 }}
             className={styles.homeLogo}
             layout
             {...props}
