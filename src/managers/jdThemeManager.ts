1743174291a1c194f1e0f58b37f09471163324a9
jeffBoom <jeff@boomreturns.com>
2023-07-16 14:58:42 -0400

    theming

--- src/managers/jdThemeManager.ts
+++ src/managers/jdThemeManager.ts
@@ -9,15 +9,18 @@ class JdThemeManager extends JdManagerBase<JdThemeStore> {
 
     public async toggleTheme(): Promise<void> {
         await this.setStateAsync({ isDarkTheme: !this.store.isDarkTheme });
+        this.setTheme();
+    }
 
+    public setTheme(): void {
         // Set bg and fg
         this.rootStyle.setProperty(primBg.key, this.store.isDarkTheme ? primBlack.value : primWhite.value);
         this.rootStyle.setProperty(primFg.key, this.store.isDarkTheme ? primWhite.value : primBlack.value);
 
         // Set shadow
         this.rootStyle.setProperty(baseShadow.key, this.store.isDarkTheme ? darkShadow.value : lightShadow.value);
-
     }
+
 }
 
 const jdThemeManager: JdThemeManager = new JdThemeManager();
--- src/views/jdAppView/jdAppViewWrapper.tsx
+++ src/views/jdAppView/jdAppViewWrapper.tsx
@@ -1,7 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import jdRoutes from "../../utils/jdRoutes"
+import { useEffect } from "react";
+import jdThemeManager from "../../managers/jdThemeManager";
 
 export function JdAppViewWrapper(): React.JSX.Element {
+
+    // Mount
+    useEffect(() => {
+        jdThemeManager.setTheme();
+    }, []);
+
     const router = createBrowserRouter([jdRoutes.registerRoutes]);
     return <RouterProvider router={router} />;
 }
